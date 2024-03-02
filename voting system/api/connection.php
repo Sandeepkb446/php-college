2dc79d24ef6b229db9f635cfa79cdbbc71e76708
Sandeepkb446 <146541458+Sandeepkb446@users.noreply.github.com>
2024-03-02 08:59:42 +0530

    Add files via upload

---
+++ voting system/api/connection.php
@@ -0,0 +1,10 @@
+<?php
+$connect = mysqli_connect("localhost", "root", "", "voting");
+if ($connect) {echo '
+    "connected!"
+    ';}
+    else {echo '
+        " not connected!"
+        ';}
+
+?>
